d1390f562c1265ac1fab2aec5312fc5cac0d5e42
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-08-06 13:49:14 -0500

    Word count complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,27 @@
 def word_count(s):
-    # Your code here
+    counts = {}
+
+    # First, we need to remove the characters we're supposed to ignore.
+    ignored_characters = ['"', ":", ";", ",", ".", "-", "+",
+                          "=", "/", "\\", "|", "[", "]", "{",
+                          "}", "(", ")", "*", "^", "&"]
+
+    for character in ignored_characters:
+        s = s.replace(character, '')
+
+    # Split the string, and make it lowercase.
+    split = s.lower().split()
+    # Iterate through the split string, adding 1
+    # to the count if it already exists, or making
+    # an entry if it doesn't.
+    for word in split:
+        if word in counts.keys():
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+        
+
 
 
 
